219a359875e4e2ad2a345dbb40581e4c69dcf063
Harshit Mandada <mandada.h@husky.neu.edu>
2020-12-03 15:45:35 +0530

    Update index.js

--- Todo_Backend/index.js
+++ Todo_Backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express()
 const cors = require('cors')
-const port = 3000
+const port = 9000
 app.use(cors());
 app.use(express.json())
 const Todos = []
